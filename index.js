6da3671791446c422ea4dbb3c99f1bf7f635f7cf
ItzSkaryet <66207296+Endternvl@users.noreply.github.com>
2020-10-20 10:37:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://legend-polar-viscose.glitch.me"]
+const urls = ["https://FunctionalBlissfulUserinterface--five-nine.repl.co"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
