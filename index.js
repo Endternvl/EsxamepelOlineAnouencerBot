fbedc9aca20105c3656d384090a3186f90711bca
ItzSkaryet <66207296+Endternvl@users.noreply.github.com>
2020-10-20 09:11:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://mica-flossy-jonquil.glitch.me"]
+const urls = ["https://legend-polar-viscose.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
